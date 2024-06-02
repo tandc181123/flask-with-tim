62ebed81e95d5cf0c50bd5d54731421ac21916a9
Tommy <153081087+tandc181123@users.noreply.github.com>
2024-06-02 18:55:43 +0800

    feat: add login & user page, update requirements

--- tutorial.py
+++ tutorial.py
@@ -1,10 +1,43 @@
-from flask import Flask, redirect, url_for, render_template
-
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
 app = Flask(__name__)
+app.secret_key = "colette300709"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content=name)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        flash("Login Successful!")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already Logged In!")
+            return redirect(url_for("user"))
+        
+        return render_template("login.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        flash(f"You have been logged out", "info")
+        session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
